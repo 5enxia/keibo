671c61f38c07eaccd316c4f6af189bf97321fe5d
takayasumorishita <c011729632@edu.teu.ac.jp>
2020-04-09 11:34:41 +0000

    Finish static pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @app_name = "keibo"
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | #{@app_name}"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | #{@app_name}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@app_name}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@app_name}"
+  end
+
+end
