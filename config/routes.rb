21c780546018f6123ba0202b1b5c14dd631cd440
takayasumorishita <c011729632@edu.teu.ac.jp>
2020-04-14 00:22:49 +0000

    change devise routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
 
+  root to: 'static_pages#home'
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
